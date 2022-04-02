b7fcb7edc0d234558f982a6dc05a2f959ef5f094
mattyo7126 <hyacinth.126@icloud.com>
2022-04-02 09:45:26 +0900

    ログインの有無で表示を変える実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,12 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と数字の両方を含めて設定してください' 
 
 
-  with_first_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name 
     validates :first_name
   end  
 
-  with_first_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end  
