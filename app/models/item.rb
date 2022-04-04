f22a6e3249e35da8ed3dae65840f4113b2107167
mattyo7126 <hyacinth.126@icloud.com>
2022-04-04 20:55:50 +0900

    Active Storageの導入

---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :shipping_price, :shipping_area_id, :shipping_date_id
+
+  validates :items_name, :items_comments, presence: true
+  validates :price, numericality: { in: 300..9999999 }, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_price_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" }
+
+
+  belongs_to :user
+  has_one_attached :image
+  # has_one :order
+end
