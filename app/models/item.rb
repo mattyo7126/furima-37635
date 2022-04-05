10bbfcfb42b0306d96ae16e1bc6ae619a4976ac5
mattyo7126 <hyacinth.126@icloud.com>
2022-04-05 10:54:06 +0900

    JSの導入（手数料・利益を表示する実装）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,27 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :shipping_price, :shipping_area, :shipping_date
-
-  validates :items_name, :items_comments, presence: true
-  validates :price, numericality: { in: 300..9999999 }, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_price_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" }
-
-
   belongs_to :user
+
+  with_options presence: true do
+    validates :items_name
+    validates :items_comments
+    validates :image
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/, message: 'には半角数数字を使用してください' }
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_price_id
+    validates :shipping_area_id
+    validates :shipping_date_id
+  end
+  
+  validates :price, numericality: true
+
   has_one_attached :image
   # has_one :order
 end
+
+
