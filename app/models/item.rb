3a6c1441b2e5f7e1a90d113f3307aa69ea5438b2
mattyo7126 <hyacinth.126@icloud.com>
2022-04-06 11:38:50 +0900

    出品時に登録した情報を表示させる

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :shipping_price, :shipping_area, :shipping_date
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_price
+  belongs_to :shipping_area
+  belongs_to :shipping_date
   belongs_to :user
   has_one_attached :image
   # has_one :order
