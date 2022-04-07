7e80e224f1bf1e5e3ebcd1c9f1cfa758c1b44ebe
mattyo7126 <hyacinth.126@icloud.com>
2022-04-07 20:15:10 +0900

    addressモデルのバリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :order
+
+  with_options presence: true do
+    validates :post_code
+    validates :prefecture_id
+    validates :city
+    validates :phone_number
+  end  
 end
