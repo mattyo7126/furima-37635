af0dac50579950e5345d703e9990ecd929bfcb5c
mattyo7126 <hyacinth.126@icloud.com>
2022-04-09 12:21:26 +0900

    addressモデルのバリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,9 +5,10 @@ class Address < ApplicationRecord
   belongs_to :order
 
   with_options presence: true do
-    validates :post_code
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ , message: 'を入力してください' }
     validates :prefecture_id
     validates :city
-    validates :phone_number
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'を数字のみで入力してください' }
   end  
 end
