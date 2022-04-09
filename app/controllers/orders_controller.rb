99df61d6b96f4eea6a8ef5673d68c4af9e70a393
mattyo7126 <hyacinth.126@icloud.com>
2022-04-09 13:09:52 +0900

    createアクションの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,18 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end  
-  
-
  
-  
+  def create
+   @order = Order.create(order_params)
+   @address = Address.create(address_params)
+   
+  end  
+  private
+
+  def order_params
+    params.require(:order).permit().merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def address_params
+    params.require(:address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(order_id: @order.id)
 end
