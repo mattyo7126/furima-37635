cea850c52915cd25dd9c83a1f3dd7b22ebf8589e
mattyo7126 <hyacinth.126@icloud.com>
2022-04-12 15:24:21 +0900

    ログアウト状態・売却済のviewの条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,10 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!
 
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
-    if current_user == @item.user
+    if current_user == @item.user || @item.order.present?
        redirect_to root_path
     end
   end  
