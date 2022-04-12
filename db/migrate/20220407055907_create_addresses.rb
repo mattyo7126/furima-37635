a61da68bf4a59eeae43fa6d5ea24035ee8bdb407
mattyo7126 <hyacinth.126@icloud.com>
2022-04-12 17:24:23 +0900

    Update 20220407055907_create_addresses.rb

--- db/migrate/20220407055907_create_addresses.rb
+++ db/migrate/20220407055907_create_addresses.rb
@@ -3,7 +3,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
     create_table   :addresses do |t|
       t.string     :post_code,                null: false
       t.integer    :prefecture_id,            null: false
-      t.string     :city,    
+      t.string     :city,                     null: false
       t.string     :address,                  null: false
       t.string     :building_name
       t.string     :phone_number,             null: false
